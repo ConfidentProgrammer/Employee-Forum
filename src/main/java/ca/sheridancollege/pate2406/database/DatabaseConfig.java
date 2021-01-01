d66df2b72bc43110799d614d76d2e49705ca4b1a
ConfidentProgrammer <deepp2618@gmail.com>
2021-01-01 18:36:21 -0500

    addedd

--- src/main/java/ca/sheridancollege/pate2406/database/DatabaseConfig.java
+++ src/main/java/ca/sheridancollege/pate2406/database/DatabaseConfig.java
@@ -2,14 +2,27 @@ package ca.sheridancollege.pate2406.database;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 public class DatabaseConfig {
+	 @Value("${spring.datasource.url}")
+	  private String dbUrl;
+
+	  @Bean
+	  public DataSource dataSource() {
+	      HikariConfig config = new HikariConfig();
+	      config.setJdbcUrl(dbUrl);
+	      return new HikariDataSource(config);
+	  }
 
 	@Bean
 	public NamedParameterJdbcTemplate namedParemterJdbcTemplate(DataSource dataSource) {
